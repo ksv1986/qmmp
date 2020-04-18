3d041357b2794a72e5c93f38aecbd6eccf14d74b
ksv1986 <ksv0x07c2@gmail.com>
2020-04-18 23:50:09 +0300

    Add option to show cover of selected track

--- src/plugins/Ui/qsui/mainwindow.h
+++ src/plugins/Ui/qsui/mainwindow.h
@@ -43,7 +43,7 @@ class QSUiAnalyzer;
 class ListWidget;
 class QSUiTabWidget;
 class QSUIQuickSearch;
-
+class CoverWidget;
 
 /**
     @author Ilya Kotov <forkotov02@ya.ru>
@@ -95,6 +95,7 @@ private slots:
     void onListChanged(int flags);
     void onCurrentPlayListChanged(PlayListModel *current, PlayListModel *previous);
     void openFileLocation();
+    void anchorChanged(int index);
 
 private:
     void closeEvent(QCloseEvent *) override;
@@ -102,6 +103,8 @@ private:
     void createWidgets();
     void createActions();
     void writeSettings();
+    CoverWidget* coverWidget() const;
+    void setCover(QString path);
     bool m_wasMaximized;
     PlayListManager *m_pl_manager;
     Ui::MainWindow m_ui;
